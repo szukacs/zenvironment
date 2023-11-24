bb2591c4754cbfe1a1d0c1b4502d4f00238d1c52
Gergely Szukács <szukacs.gergely@gmail.com>
2023-11-24 21:27:03 +0100

    query added

---
+++ frontend/zenvironment-frontend/src/components/mygardenQueries.ts
@@ -0,0 +1,13 @@
+import {api} from "@/lib/api/api";
+import {useQuery} from "@tanstack/react-query";
+
+export const myGardenQueryKeys = {
+  root: () => ['mygarden'] as const
+} as const;
+
+export function useGetMyGardenQuery() {
+  return useQuery({
+    queryKey: myGardenQueryKeys.root(),
+    queryFn: () => api.myGarden.getMyGarden(),
+  });
+}
