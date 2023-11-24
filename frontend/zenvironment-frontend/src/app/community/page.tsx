6ae685504cdeb056219376a294d78f1f37e90b48
Gergely Szukács <szukacs.gergely@gmail.com>
2023-11-24 22:37:08 +0100

    my community wired in

--- frontend/zenvironment-frontend/src/app/community/page.tsx
+++ frontend/zenvironment-frontend/src/app/community/page.tsx
@@ -3,8 +3,20 @@
 import { Challenges } from "@/components/Challenges";
 import { Page } from "@/components/Page";
 import { DisplayTab, SimpleTab } from "@/components/SimpleTab";
+import {useGetMyCommunityQuery} from "@/components/mygardenQueries";
+import {Box, CircularProgress} from "@mui/material";
 
 export default function Community() {
+  const myCommunityQuery = useGetMyCommunityQuery()
+
+  if (myCommunityQuery.isLoading) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress color="success"/>
+      </Box>
+    )
+  }
+
   const tabs: DisplayTab[] = [
     {
       label: "Timeline",
@@ -26,6 +38,13 @@ export default function Community() {
 
   return (
     <Page title="Community">
+      {myCommunityQuery.status == 'error' && (<>Error, this is definitely not our fault! Drop out your laptop and buy a new
+        one! :)</>)}
+      {myCommunityQuery.status == 'success' && (
+        <>
+          {myCommunityQuery.data.data.name}
+        </>
+      )}
       <SimpleTab tabs={tabs} />
     </Page>
   );
