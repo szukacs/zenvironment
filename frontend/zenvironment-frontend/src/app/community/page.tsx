ba1734b2e772f2c6e84212e2609b67966ad577a7
Gergely Szukács <szukacs.gergely@gmail.com>
2023-11-24 20:56:43 +0100

    tab done

--- frontend/zenvironment-frontend/src/app/community/page.tsx
+++ frontend/zenvironment-frontend/src/app/community/page.tsx
@@ -1,17 +1,27 @@
 "use client"
 
-import { Page } from "@/components/Page";
+import {Page} from "@/components/Page";
 import {DisplayTab, SimpleTab} from "@/components/SimpleTab";
 
 export default function Community() {
-  const tabs: DisplayTab[] = [{
-    label: "Example 1",
-    content: <>Example tab 1</>
-  },
+  const tabs: DisplayTab[] = [
     {
-      label: "Example 2",
+    label: "Timeline",
+    content: <>Example tab 1</>
+    },
+    {
+      label: "Gardens",
       content: <>Example tab 2</>
-    }]
+    },
+    {
+      label: "Challenges",
+      content: <>Example tab 3</>
+    },
+    {
+      label: "Market",
+      content: <>Example tab 4</>
+    }
+  ]
 
   return (
     <Page title="Community">
