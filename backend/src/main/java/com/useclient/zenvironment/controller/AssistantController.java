11ba0ef37d11e8ce1b82ba7053ca1356536ddd04
spiglebach <spiglebach@gmail.com>
2023-11-25 15:43:39 +0100

    assistant chat with history

--- backend/src/main/java/com/useclient/zenvironment/controller/AssistantController.java
+++ backend/src/main/java/com/useclient/zenvironment/controller/AssistantController.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/assistant")
 @RequiredArgsConstructor
@@ -19,17 +17,11 @@ public class AssistantController {
     @PostMapping("/chat")
     public ResponseEntity<Message> askForAssistance(@RequestBody Message chatRequest) {
         try {
-            var response = assistantService.getAssistance(chatRequest.getMessage());
-            return ResponseEntity.ok(new Message(response));
+            var response = assistantService.getAssistance(chatRequest);
+            return ResponseEntity.ok(response);
         } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
         }
-    }
-
-    @Transactional(readOnly = true)
-    @GetMapping("/suggestions")
-    public ResponseEntity<List<String>> getQuickQuestions() {
-        return ResponseEntity.ok(assistantService.getGardenBasedAssistanceQuestions());
     }
 
 }
