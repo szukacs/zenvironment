8bf4c9d5f6bdb52797ecc2bef0ef56051bef4ed8
Gergely Szukács <szukacs.gergely@gmail.com>
2023-11-25 19:26:49 +0100

    garden stat added

--- frontend/zenvironment-frontend/src/components/GardenCard.tsx
+++ frontend/zenvironment-frontend/src/components/GardenCard.tsx
@@ -30,35 +30,41 @@ export const GardenCard: React.FC<GardenCardProps> = ({ garden }) => {
         <Box pt={2}>
           <Garden isAddDisabled tileWidth={60} plants={garden.plants ?? []} />
         </Box>
-        <Typography gutterBottom variant="h5" component="div">
-          {garden.name}
-        </Typography>
-        <Stack spacing={1}>
-          {(garden.plantSummaries?? []).map(summary => (
-            <Stack
-              direction="row"
-              justifyContent="flex-start"
-              alignItems="center"
-              spacing={2}
-            >
-              <Box
-                component="img"
-                sx={{
-                  maxWidth: "100%",
-                  width: 30,
-                  objectFit: "contain",
-                  marginRight: 1,
-                }}
-                src={`${baseURL}${summary.plantType?.imageUrl}`}
-              ></Box>
-              <Typography>x</Typography>
-              <Typography sx={{color: "#cfa448"}}>{`${summary.plantCount} pcs`}</Typography>
-              <Typography sx={{width: '95px', color: "#52c454"}}>{`CO2 ${roundNumber(summary.allFixatedCO2InKilograms ?? 0)} kg`}</Typography>
-              <Typography sx={{width: '95px', color: "#34c0eb"}}>{`O2 ${roundNumber(summary.allProducedOxygenInKilograms ?? 0)} kg`}</Typography>
-            </Stack>
-          ))}
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {garden.name}
+            </Typography>
+            <Stack spacing={1}>
+              {(garden.plantSummaries?? []).map(summary => (
+                <Stack
+                  key={summary.plantType?.id}
+                  direction="row"
+                  justifyContent="flex-start"
+                  alignItems="center"
+                  spacing={1}
+                >
+                  <Box
+                    component="img"
+                    sx={{
+                      maxWidth: "100%",
+                      width: 30,
+                      objectFit: "contain",
+                      marginRight: 1,
+                    }}
+                    src={`${baseURL}${summary.plantType?.imageUrl}`}
+                  ></Box>
+                  <Typography>x</Typography>
+                  <Typography sx={{color: "#cfa448"}}>{`${summary.plantCount} pcs`}</Typography>
+                  <Typography sx={{width: '95px', color: "#52c454"}}>{`CO2 ${roundNumber(summary.allFixatedCO2InKilograms ?? 0)} kg`}</Typography>
+                  <Typography sx={{width: '95px', color: "#34c0eb"}}>{`O2 ${roundNumber(summary.allProducedOxygenInKilograms ?? 0)} kg`}</Typography>
+                </Stack>
+              ))}
 
-        </Stack>
+            </Stack>
+          </CardContent>
+        </Card>
+
       </CardContent>
     </Card>
   );
