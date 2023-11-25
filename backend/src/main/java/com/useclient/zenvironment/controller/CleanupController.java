3c004ffc24e9907dbf3bacfbffdf8758dc70ebab
Pityu Szivos <pityu.django@gmail.com>
2023-11-25 19:04:51 +0100

    add messages to swagger in controllers

--- backend/src/main/java/com/useclient/zenvironment/controller/CleanupController.java
+++ backend/src/main/java/com/useclient/zenvironment/controller/CleanupController.java
@@ -3,6 +3,7 @@ package com.useclient.zenvironment.controller;
 import com.useclient.zenvironment.model.dao.Garden;
 import com.useclient.zenvironment.repository.GardenRepository;
 import com.useclient.zenvironment.repository.PlantRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ public class CleanupController {
     private final GardenRepository gardenRepository;
     private final PlantRepository plantRepository;
 
+    @Operation(summary = "A technical endpoint to delete all gardens unnecessarily generated", description = "Returns with a status code ok in case everything went right")
     @Transactional
     @DeleteMapping
     public ResponseEntity<Void> deleteGeneratedGardens() {
--- backend/src/main/java/com/useclient/zenvironment/controller/ZenController.java
+++ backend/src/main/java/com/useclient/zenvironment/controller/ZenController.java
@@ -6,6 +6,7 @@ import com.useclient.zenvironment.service.ChallengeService;
 import com.useclient.zenvironment.service.ExchangeService;
 import com.useclient.zenvironment.service.GardenService;
 import com.useclient.zenvironment.service.ZenService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +27,7 @@ public class ZenController {
     private final ExchangeService exchangeService;
     private final GardenService gardenService;
 
+    @Operation(summary = "Get all available plant types in the app", description = "Returns all plant types with associated information")
     @Transactional(readOnly = true)
     @GetMapping("/plant-types")
     public ResponseEntity<List<PlantTypeDto>> getPlantTypes() {
@@ -35,6 +37,7 @@ public class ZenController {
         return ResponseEntity.ok(responseBody);
     }
 
+    @Operation(summary = "Creates a garden with required parameters", description = "Returns newly created garden")
     @Transactional
     @PostMapping("/gardens")
     public ResponseEntity<GardenDto> createGarden(@RequestBody NewGardenDto newGardenDto) {
@@ -42,6 +45,7 @@ public class ZenController {
         return ResponseEntity.ok(mapper.toDto(newGarden));
     }
 
+    @Operation(summary = "Gets garden by unique gardenId", description = "Returns garden belonging to id")
     @Transactional(readOnly = true)
     @GetMapping("/gardens/{gardenId}")
     public ResponseEntity<GardenDto> getGardenById(@PathVariable String gardenId) {
@@ -50,7 +54,7 @@ public class ZenController {
         return ResponseEntity.ok(responseBody);
     }
 
-
+    @Operation(summary = "Creates new plant in given garden", description = "Returns newly created plant")
     @Transactional
     @PostMapping("/gardens/{gardenId}/plants")
     public ResponseEntity<PlantDto> addPlant(@PathVariable String gardenId, @RequestBody NewPlantDto newPlantDto) {
@@ -60,6 +64,7 @@ public class ZenController {
         return ResponseEntity.ok(responseBody);
     }
 
+    @Operation(summary = "Find user's garden", description = "Returns user's garden")
     @Transactional(readOnly = true)
     @GetMapping("/my-garden")
     public ResponseEntity<GardenDto> getMyGarden() {
@@ -68,6 +73,7 @@ public class ZenController {
         return ResponseEntity.ok(responseBody);
     }
 
+    @Operation(summary = "Initiating harvest for user's garden", description = "Return harvest details")
     @Transactional(readOnly = true)
     @GetMapping("/my-garden/harvests")
     public ResponseEntity<List<HarvestSummary>> getMyHarvests() {
@@ -76,6 +82,7 @@ public class ZenController {
         return ResponseEntity.ok(harvestSummary);
     }
 
+    @Operation(summary = "Creates new plant in given garden", description = "Returns newly created plant")
     @Transactional
     @PostMapping("/my-garden/plants")
     public ResponseEntity<PlantDto> addPlant(@RequestBody NewPlantDto newPlantDto) {
@@ -93,6 +100,7 @@ public class ZenController {
         return ResponseEntity.ok(responseBody);
     }
 
+    @Operation(summary = "Harvesting plant based on user input", description = "Returns harvesting details")
     @Transactional
     @PostMapping("/my-garden/plants/{plantId}/harvest")
     public ResponseEntity<HarvestDto> harvestPlant(@PathVariable String plantId, @RequestBody NewHarvestDto newHarvestDto) {
@@ -102,6 +110,7 @@ public class ZenController {
         return ResponseEntity.ok(responseBody);
     }
 
+    @Operation(summary = "Initiating finding the community belonging to the user", description = "Returns community to which user belongs to")
     @Transactional(readOnly = true)
     @GetMapping("/my-community")
     public ResponseEntity<CommunityDto> getMyCommunity() {
@@ -110,6 +119,7 @@ public class ZenController {
         return ResponseEntity.ok(responseBody);
     }
 
+    @Operation(summary = "Finding challenges for the community", description = "Returns back actual state of the challenges of the user's commmunity")
     @Transactional(readOnly = true)
     @GetMapping("/my-community/challenges")
     public ResponseEntity<List<ChallengeDto>> getMyCommunityChallenges() {
@@ -119,6 +129,7 @@ public class ZenController {
         return ResponseEntity.ok(responseBody);
     }
 
+    @Operation(summary = "User creates a new exchange request to give away plants", description = "Returns back all exchange requests to which the user is affiliated to")
     @Transactional
     @PostMapping("my-garden/exchanges")
     public ResponseEntity<List<ExchangeDto>> addExchange(ExchangeDto exchangeDto){
@@ -126,6 +137,7 @@ public class ZenController {
         return ResponseEntity.ok(responseBody);
     }
 
+    @Operation(summary = "Finding all exchanges belonging to given garden", description = "Returns all exchanges to which user is affiliated to")
     @Transactional(readOnly = true)
     @GetMapping("/my-garden/exchanges/{gardenId}")
     public ResponseEntity<List<ExchangeDto>> findAllExchangesBelongingToGarden(@PathVariable("gardenId") String gardenId){
@@ -133,6 +145,7 @@ public class ZenController {
         return ResponseEntity.ok(responseBody);
     }
 
+    @Operation(summary = "User can accept another user's exchange request", description = "Return a success message tha request was accepted by user")
     @Transactional
     @PutMapping("my-garden/exchanges")
     public ResponseEntity<?> acceptExchange(ExchangeDto exchangeDto){
@@ -140,6 +153,7 @@ public class ZenController {
         return ResponseEntity.ok("You successfully accepted the plants");
     }
 
+    @Operation(summary = "Finding all exchanges belonging to given community", description = "Returns all exchanges within the community")
     @Transactional(readOnly = true)
     @GetMapping("/my-community/exchanges/{communityId}")
     public ResponseEntity<List<ExchangeDto>> findAllExchangesBelongingToCommuniy(@PathVariable("communityId") String communityId){
